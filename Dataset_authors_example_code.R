a6dcfa8b1953655f672e0cf359a86bd9da465629
Joy Kumagai <49419490+jkumagai96@users.noreply.github.com>
2022-01-31 10:49:17 +0100

    + affiliation

--- Dataset_authors_example_code.R
+++ Dataset_authors_example_code.R
@@ -1,4 +1,5 @@
-# Joy Kumagai
+# Joy Kumagai(joy.kumagai@senckenberg.de)
+# Technical Support Unit on Knowledge and Data
 # Example of authors dataset
 
 df <- data.frame(Chapter = character(),
@@ -7,6 +8,7 @@ df <- data.frame(Chapter = character(),
                  Full_Name = character(),
                  Role = character(),
                  Orcid = numeric(),
+                 Affiliation = character(),
                  Country1 = character(),
                  Country2 = character())
 write.csv(df, "IPBES/Assessments/Dataset_authors_example_v1.csv", fileEncoding = "UTF-8", row.names = F)
